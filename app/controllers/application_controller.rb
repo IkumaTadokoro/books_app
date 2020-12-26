8c1741dbb13c96adcdb6e7c9f32b32005b5851a1
IkumaTadokoro <tadokorodev@gmail.com>
2020-12-26 17:16:31 +0900

    ユーザの属性に住所・郵便番号・自己紹介を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:address, :postal_code, :self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:address, :postal_code, :self_introduction])
+  end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
