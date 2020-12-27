d2adaa0a402a40f27ff5899ea25bbc38123794c0
IkumaTadokoro <tadokorodev@gmail.com>
2020-12-27 22:25:29 +0900

    開発環境でのメール送信方法にgem:letter-opener-webを使用

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :books
   root to: "books#index"
   resources :users, only: [:show]
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
