18c8a773e164aff9be4207eab7887c55b0865b5a
IkumaTadokoro <tadokorodev@gmail.com>
2021-01-06 18:11:32 +0900

    定数`ADDITIONAL_PARAMETERS`を変数`keys`に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  ADDITIONAL_PRAMETERS = %i[address postal_code self_introduction]
-
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
   before_action :authenticate_user!
@@ -10,8 +8,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: ADDITIONAL_PRAMETERS)
-    devise_parameter_sanitizer.permit(:account_update, keys: ADDITIONAL_PRAMETERS)
+    keys = %i[address postal_code self_introduction]
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
   end
 
   def set_locale
